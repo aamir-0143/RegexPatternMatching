fe2eedecf4fe345421dd9b1d817cef1596d4fca0
Md aamir reza <aamirreza156@gamil.com>
2021-12-17 21:27:36 +0530

    [Aamir19] Add : Added Enter A Valid First Name

---
+++ RegexPatternMatching/Pattern.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegexPatternMatching
+{
+    public class Pattern 
+    {
+        public static string FIRST_NAME_REGEX = "^[A-Z]{1}[A-Za-z]{2,}$"; //Minimum 3 Characters
+
+        //Method For Validate First Name
+        public bool validateFirstName(string fname) 
+        {
+            return Regex.IsMatch(fname, FIRST_NAME_REGEX);
+        }
+
+    }
+}
+
---
+++ RegexPatternMatching/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RegexPatternMatching 
+{ 
+    class Program
+    {
+        static void Main(string[] args) 
+        { 
+            string instr = Console.ReadLine();
+            Pattern program = new Pattern();
+            Console.WriteLine(program.validateFirstName(instr));
+        }
+    }
+}
+
